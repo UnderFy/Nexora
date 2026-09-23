446f061a3d65c7837d0f0330c44a700cb00f6e93
Murilo Novais <murilonovais016@gmail.com>
2026-09-23 01:21:41 -0300

    Initialize Supabase client with URL and key

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://qkvijvkxnkadxipdwcel.supabase.co";
+const SUPABASE_KEY = "sb_publishable_fxXLtlg78qrdqZaQhjUJQw_XoNhtuSK";
 
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
